7a6ee1155c0037cd05d94f547df9643bbd34c749
Jan Alinio <janlloydalinio99@gmail.com>
2026-02-26 22:06:19 +0800

    Add palindrome checker C++ code

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+};
+
+int main() {
+    Solution s;
+    int x;
+    cin >> x;
+
+    if (s.isPalindrome(x))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
